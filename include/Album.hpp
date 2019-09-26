45c6b97d961da6bc56443b40c0a349065cd818c2
Tiago Cruz <tiago.unity.cruz@gmail.com>
2019-09-26 14:49:17 -0300

    getter e setter do Album

--- include/Album.hpp
+++ include/Album.hpp
@@ -16,6 +16,17 @@ class Album{
         Album();
         Album(string nome, int duracao, int ano, int qtd);
         void imprimeNoArquivo(ofstream &outfiles);
+        void setQtdMusicas(int qtdMusicas);
+        int getQtdMusicas();
+        void setNome(string nome);
+        string getNome();
+        void setDuracao(int duracao);
+        int getDuracao();
+        void setAnoLancamento(int anoLancamento);
+        int getAnoLancamento();
+        void setMusicas(vector<Musica*> musicas);
+        vector<Musica*> getMusica();
 };
+
 
 #endif
--- src/Album.cpp
+++ src/Album.cpp
@@ -11,3 +11,43 @@ Album::Album(string nome, int duracao, int ano, int qtd){
 void Album::imprimeNoArquivo(ofstream &outfile){
     //
 }
+
+void Album::setQtdMusicas(int qtdMusicas) {
+    this->qtdMusicas = qtdMusicas;
+}
+
+int Album::getQtdMusicas() {
+    return qtdMusicas;
+}
+
+void Album::setNome(string nome) {
+    this->nome = nome;
+}
+
+string Album::getNome() {
+    return nome;
+}
+
+void Album::setDuracao(int duracao) {
+    this->duracao = duracao;
+}
+
+int Album::getDuracao() {
+    return duracao;
+}
+
+void Album::setAnoLancamento(int anoLancamento) {
+    this->anoLancamento = anoLancamento;
+}
+
+int Album::getAnoLancamento() {
+    return anoLancamento;
+}
+
+void Album::setMusicas(vector<Musica*> musicas){
+    this->musicas = musicas;
+}
+
+vector<Musica*> Album::getMusica(){
+    return this->musicas;
+}
