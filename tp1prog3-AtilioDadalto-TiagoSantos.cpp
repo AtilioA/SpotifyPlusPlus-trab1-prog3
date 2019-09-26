204e3ece2ab8b31fc73e6a2c48f7fa9974444d4a
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2019-09-26 07:16:19 -0300

    opções de execução

--- tp1prog3-AtilioDadalto-TiagoSantos.cpp
+++ tp1prog3-AtilioDadalto-TiagoSantos.cpp
@@ -4,7 +4,31 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /* Validação de argumentos de entrada */
+    for(int i = 1; argv[i] != NULL; i++){
+        if(argv[i][0] == '-'){
+            switch(argv[i][1]){
+                case 'g':
+                    cout << "--GENERO-- " << argv[i + 1] << endl;
+                    i++;
+                    break;
+                case 'u':
+                    cout << "--USUARIO-- " << argv[i + 1] << endl;
+                    i++;
+                    break;
+                case 'm':
+                    cout << "--MIDIA-- " << argv[i + 1] << endl;
+                    i++;
+                    break;
+                case 'f':
+                    cout << "--FAVORITOS-- " << argv[i + 1] << endl;
+                    i++;
+                    break;
+                default:
+                    cerr << "OPÇÃO NÃO RECONHECIDA" << endl;
+                    break;
+            }
+        }
+    }
     if (argv[1] == NULL)
     {
         printf("Entrada de buscas invalida.\n");
