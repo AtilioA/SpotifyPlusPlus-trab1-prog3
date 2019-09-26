b0d99e71bd3bee6d65113df6554add06ff990003
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2019-09-26 07:23:38 -0300

    opções de execução

--- tp1prog3-AtilioDadalto-TiagoSantos.cpp
+++ tp1prog3-AtilioDadalto-TiagoSantos.cpp
@@ -4,23 +4,31 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    string gen;
+    string user;
+    string midia;
+    string favs;
     for(int i = 1; argv[i] != NULL; i++){
         if(argv[i][0] == '-'){
             switch(argv[i][1]){
                 case 'g':
                     cout << "--GENERO-- " << argv[i + 1] << endl;
+                    gen = argv[i + 1];
                     i++;
                     break;
                 case 'u':
                     cout << "--USUARIO-- " << argv[i + 1] << endl;
+                    user = argv[i + 1];
                     i++;
                     break;
                 case 'm':
                     cout << "--MIDIA-- " << argv[i + 1] << endl;
+                    midia = argv[i + 1];
                     i++;
                     break;
                 case 'f':
                     cout << "--FAVORITOS-- " << argv[i + 1] << endl;
+                    favs = argv[i + 1];
                     i++;
                     break;
                 default:
@@ -29,6 +37,7 @@ int main(int argc, char *argv[])
             }
         }
     }
+    cout << "|usuarios: " << user << "| |genero: " << gen << "| |midia: " << midia << "| |favoritos: " << favs << "|" << endl;
     if (argv[1] == NULL)
     {
         printf("Entrada de buscas invalida.\n");
