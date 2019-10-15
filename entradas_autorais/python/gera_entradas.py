feb5ea26430b19e18eff77d21c1bf990a2330f94
AtilioA <atiliodadalto@hotmail.com>
2019-10-14 23:28:39 -0300

    Leves melhorias e correções no gerador

--- entradas_autorais/python/gera_entradas.py
+++ entradas_autorais/python/gera_entradas.py
@@ -5,22 +5,22 @@ from faker import Faker
 fake = Faker('pt_BR')
 
 
-def gera_usuarios(n):
+def gera_usuarios(nUsuarios):
     tipos = ['A', 'P', 'U']
     with open("usuarios_python.csv", "w+", encoding="utf8") as file:
-        file.write("Codigo;Tipo;Nome\n")
-        for i in range(1, n + 1):
+        file.write("Código;Tipo;Nome\n")
+        for i in range(1, nUsuarios + 1):
             file.write(f"{i};{random.choice(tipos)};{names.get_full_name()}\n")
     pass
 
 
-def gera_generos(n):
+def gera_generos(nGeneros):
     with open("genres.txt", "r", encoding="utf8") as fG:
         genres = [line.strip().capitalize() for line in fG]
 
     with open("generos_python.csv", "w+", encoding="utf8") as file:
         file.write("Sigla;Nome\n")
-        for i in range(n):
+        for i in range(nGeneros):
             genero = random.choice(genres)
             genres.remove(genero)
             if len(genero.split()) > 1:
@@ -31,7 +31,7 @@ def gera_generos(n):
     pass
 
 
-def gera_midias(n, arquivoGeneros, arquivoUsuarios):
+def gera_midias(nMidias, arquivoGeneros, arquivoUsuarios):
     with open("midias_python.csv", "w+", encoding="utf8") as file:
         file.write("Código;Nome;Tipo;Produtores;Duração;Gênero;Temporada;Álbum;Publicação\n")
 
@@ -42,8 +42,8 @@ def gera_midias(n, arquivoGeneros, arquivoUsuarios):
         tipoUsuario = usuariosCSV["Tipo"].tolist()
         nomeUsuario = usuariosCSV["Nome"].tolist()
 
-        nMidias = range(1, n + 1)
-        for i in nMidias:
+        rangeMidias = range(1, nMidias + 1)
+        for i in rangeMidias:
             file.write(f"{i};")
             nomeMidia = fake.catch_phrase()
             publicacaoMidia = random.randint(1990, 2077)
@@ -56,7 +56,7 @@ def gera_midias(n, arquivoGeneros, arquivoUsuarios):
                 temporadaMidia = ''
                 albumMidia = fake.catch_phrase()
                 while len(produtores) < random.randint(1, 3):
-                    randProd = random.randint(0, n / 2 - 1)
+                    randProd = random.randint(1, nUsuarios - 1)
                     if tipoUsuario[randProd] is "A":
                         produtores.append(randProd + 1)
             else:
@@ -64,7 +64,7 @@ def gera_midias(n, arquivoGeneros, arquivoUsuarios):
                 temporadaMidia = random.randint(1, 20)
                 albumMidia = ''
                 while len(produtores) < random.randint(1, 3):
-                    randProd = random.randint(0, n / 2 - 1)
+                    randProd = random.randint(1, nUsuarios - 1)
                     if tipoUsuario[randProd] is "P":
                         produtores.append(randProd + 1)
 
@@ -81,22 +81,27 @@ def gera_midias(n, arquivoGeneros, arquivoUsuarios):
     pass
 
 
-def gera_favoritos(n):
+def gera_favoritos(nFavoritos, nMidias):
     with open("favoritos_python.csv", "w+", encoding="utf8") as file:
-        file.write("Codigo;Midias Favoritas\n")
-        for i in range(1, n + 1):
+        file.write("Código;Mídias Favoritas\n")
+        for i in range(1, nFavoritos + 1):
             file.write(f"{i};")
-            nMidias = range(random.randrange(n / 5))
-            for f in nMidias:
-                if f != nMidias[-1]:
-                    file.write(f"{random.randrange(1, n)},")
+            rangeMidias = range(random.randrange(0, 20))
+            for f in rangeMidias:
+                if f != rangeMidias[-1]:
+                    file.write(f"{random.randrange(1, nMidias)},")
                 else:
-                    file.write(f"{random.randrange(1, n)}")
+                    file.write(f"{random.randrange(1, nMidias)}")
             file.write("\n")
     pass
 
+# Editar valores aqui
+nUsuarios = 100
+nFavoritos = nUsuarios
+nMidias = 500
+nGeneros = 30
 
-gera_favoritos(200)
-gera_usuarios(200)
-gera_generos(200)
-gera_midias(200, "generos_python.csv", "usuarios_python.csv")
+gera_favoritos(nFavoritos, nMidias)
+gera_usuarios(nUsuarios)
+gera_generos(nGeneros)
+gera_midias(nMidias, "generos_python.csv", "usuarios_python.csv")
